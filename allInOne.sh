f67aa8587ab62d48e57e07e89aafcfb1e20b7617
Rick Hallett <rickhallett@live.co.uk>
2020-10-06 13:52:34 +0100

    testing alias.acp

--- allInOne.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-# read projects current directory with $PWD
-echo “running command from” $PWD
-cd $PWD
-git add .
-echo “Enter commit message: “
-git commit -am “$commitMessage”
-git push origin master
